3618ecd22004f31af53d33f251c308055059d0a2
Lonolf <48058118+Lonolf@users.noreply.github.com>
2021-01-10 10:38:03 +0100

    Update index.js
    Add hotReloading for functions, reducers and actions
    Added the possibility to pass a function or action to the dispatch and call functions
    Added a console.log of calls

--- src/logic/index.js
+++ src/logic/index.js
@@ -1,15 +1,42 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
 
 import slicers from './slicers'
-import functions from './functions'
+import callableFunctions from './functions'
 
-const actions = slicers.reduce((list, slicer) => ({ ...list, ...slicer.actions }), {})
+let functions = callableFunctions
+let actions = slicers.reduce((list, slicer) => ({ ...list, ...slicer.actions }), {})
 const reducer = slicers.reduce((list, slicer) => ({ ...list, [slicer.name]: slicer.reducer }), {})
 
-export const store = configureStore({ reducer })
+export const store = configureStore({
+  reducer,
+  devTools: process.env.REACT_APP_ENV !== 'production',
+})
+
+if (process.env.REACT_APP_ENV === 'local' && module.hot) {
+  module.hot.accept('./slicers', () => {
+    try {
+      const slicers = require('./slicers').default
+      const newRootReducer = combineReducers(slicers.reduce((list, slicer) => ({ ...list, [slicer.name]: slicer.reducer }), {}))
+      actions = slicers.reduce((list, slicer) => ({ ...list, ...slicer.actions }), {})
+      store.replaceReducer(newRootReducer)
+    } catch (error) {
+      console.error(error)
+    }
+  })
+  module.hot.accept('./functions', () => {
+    try {
+      functions = require('./functions').default
+    } catch (error) {
+      console.error(error)
+    }
+  })
+}
 
 export const dispatch = (name, payload) => {
   try {
+    if (typeof name === 'function')
+      store.dispatch([name](payload))
     if (typeof actions?.[name] === 'function')
       store.dispatch(actions[name](payload))
     else
@@ -20,14 +47,27 @@ export const dispatch = (name, payload) => {
 }
 
 export const call = async(name, payload = {}) => {
+  let response = false
   dispatch('startLoading', ({ loadingId: name, payload }))
+  if (process.env.REACT_APP_ENV !== 'production')
+    console.log('called ' + name, payload)
   try {
-    if (typeof functions?.[name] === 'function')
-      await functions[name]({ payload, dispatch })
+    const { getState } = store
+    if (typeof name === 'function')
+      response = await [name]({ payload, dispatch, getState, call })
+    if (typeof functions?.[String(name)] === 'function')
+      response = await functions[name]({ payload, dispatch, getState, call })
     else
-      throw new Error(`Function "${name}" not existing`)
+      throw new Error(`Function "${String(name)}" not existing`)
   } catch (error) {
+    dispatch('createError', error.message)
     console.error(error)
   }
   dispatch('stopLoading', name)
+  return response
 }
+
+const onStart = async() => {
+}
+
+onStart()
